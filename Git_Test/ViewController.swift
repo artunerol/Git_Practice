04c8c93ee90b052b661a1442bb68637fd7053201
Artun <artun.erol@yahoo.com>
2021-09-29 00:36:35 +0300

    test

--- Git_Test/ViewController.swift
+++ Git_Test/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
+        
         print("This is dev branch")
         print("This is it")
         // Do any additional setup after loading the view.
